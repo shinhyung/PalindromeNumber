b68fbc3d3e210198d2633e5a73307b1916f58cb0
shinhyung <mazide23@cnu.ac.kr>
2019-01-07 18:19:43 +0900

    first commit

--- MainClass.java
+++ MainClass.java
@@ -4,38 +4,39 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public int[] intersect(int[] nums1, int[] nums2) {
         
-        int count=0;  
-        ArrayList<Integer> numbers = new ArrayList<>();
-       
-        
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i]==1) {
-               count++;
-            }
-            else{
-                numbers.add(count);
-                count=0;
-            }
-            
-        }
-        
-        numbers.add(count);
-        int max=numbers.get(0);
-        
-        for(int i=1;i<numbers.size();i++){
-            if(numbers.get(i)>max){
-                max = numbers.get(i);
-            }
-            else {
-            
-            }
-        }  
-        return max;
-    }
+    	ArrayList<Integer> list = new ArrayList<>();
     	
-}
+    	int i=0;
+    	int j=0;
+    	
+       while(i<nums1.length&&j<nums2.length) {
+    	  
+    	   if(nums1[i] < nums2[j]) { 	
+           	i++;
+           	
+           }
+           else if(nums2[j]==nums1[i]) {
+           	list.add(nums1[i]);
+           	i++;
+           	j++;
+           }
+           else {
+           	j++;
+           }
+       }
+        //두 배열에 표시된 횟수를 알아야 한다.
+       
+        int[] array = new int[list.size()];
+        
+        for(int f=0;f<array.length;f++) {
+           array[f] = list.get(f).intValue();
+        }
+        return array;
+    }
+    }
+
 public class MainClass {
     public static int[] stringToIntegerArray(String input) {
         input = input.trim();
@@ -53,15 +54,34 @@ public class MainClass {
         return output;
     }
     
+    public static String integerArrayToString(int[] nums, int length) {
+        if (length == 0) {
+            return "[]";
+        }
+    
+        String result = "";
+        for(int index = 0; index < length; index++) {
+            int number = nums[index];
+            result += Integer.toString(number) + ", ";
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static String integerArrayToString(int[] nums) {
+        return integerArrayToString(nums, nums.length);
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = in.readLine()) != null) {
-            int[] nums = stringToIntegerArray(line);
+            int[] nums1 = stringToIntegerArray(line);
+            line = in.readLine();
+            int[] nums2 = stringToIntegerArray(line);
             
-            int ret = new Solution().findMaxConsecutiveOnes(nums);
+            int[] ret = new Solution().intersect(nums1, nums2);
             
-            String out = String.valueOf(ret);
+            String out = integerArrayToString(ret);
             
             System.out.print(out);
         }
