5df71a24d2412034ce1083f39e532a191fa3e844
shinhyung <mazide23@cnu.ac.kr>
2019-01-28 20:04:10 +0900

    first commit

---
+++ ListNode.java
@@ -0,0 +1,10 @@
+package Solution;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) 
+	{ 
+		val = x; 
+	}
+}
--- MainClass.java
+++ MainClass.java
@@ -1,100 +1,124 @@
 package Solution;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* -----------------------------------
+ *  WARNING:
+ * -----------------------------------
+ *  Your code may fail to compile
+ *  because it contains public class
+ *  declarations.
+ *  To fix this, please remove the
+ *  "public" keyword from your class
+ *  declarations.
+ */
+
+
+//Definition for singly-linked list.
+
 
 class Solution {
-	public boolean isPalindrome(int x) {
-	    
-		int count=1;
-		ArrayList<Integer> listone = new ArrayList<Integer>();
-		ArrayList<Integer> listtwo = new ArrayList<Integer>();
-		String token = Integer.toString(x);
-		String[] tokenlist = token.split("");
+
+	public boolean isPalindrome(ListNode head) {
+	   
+		ArrayList<Integer> list = new ArrayList<Integer>();
+	    ArrayList<Integer> listtwo = new ArrayList<Integer>();
+        ArrayList<Integer> listthree = new ArrayList<Integer>();
+
+	    int count=1;
 		
-		
-		
-		if(x<0) {
-			return false;
-		}
-		if(x<=9) {
+	    if(head==null) {
 			return true;
 		}
-		
-		else {
-			
-				for(int k=1;k<tokenlist.length;k++) {
-					if(tokenlist[k].equals(tokenlist[k-1]))
-					count++;
-				}
-				if(count==tokenlist.length) { //숫자가 전부 동일할 경우
-					return true;
-				}
-				else{ //숫자가 전부 동일하지 않을 경우
-					count=0;
-					for(int k=1;k<tokenlist.length-1;k++) {
-						count++; 
-					}
-					if(tokenlist[0].equals(tokenlist[tokenlist.length-1])&&count==1) { // 양 끝에 숫자는 같고 가운데 숫자가 하나일 경우
-						return true;
-					}
-					count=1;
-					for(int k=1;k<tokenlist.length-1;k++) {
-					if(k+1!=tokenlist.length&&tokenlist[k].equals(tokenlist[k+1])) {
-							count++;
-					}
-					}
-					if(tokenlist[0].equals(tokenlist[tokenlist.length-1])&&count==tokenlist.length-2) {
-						//첫 번째와 끝의 숫자가 동일하면서 안에 숫자가 전부 동일
-						return true;
-					}
-					else {
-						for(int j=0;j<tokenlist.length/2;j++) {
-							int sum = Integer.parseInt(tokenlist[j]);
-							listone.add(sum);
-						}
-						for(int j=tokenlist.length-1;j>=tokenlist.length/2;j--) {
-							int sum = Integer.parseInt(tokenlist[j]);
-							listtwo.add(sum);
-						}
-						count=0;
-						for(int j=0;j<listone.size();j++) {
-							if(listone.get(j).equals(listtwo.get(j))) {
-								count++;
-							}
-						}
-						if(tokenlist[0].equals(tokenlist[tokenlist.length-1])&&count==listone.size()) {
-							return true;
-						}
-						
-						return false;
-					}
-				}
-				
-			}
-		
 	    
-	}
-	
+	    while(head.next!=null) {
+			list.add(head.val);
+			head=head.next;
+		}
+	    list.add(head.val);
+	    
+	    for(int k=0;k<list.size();k++) {
+	    	count++;
+	    }
+	    if(count==1) {
+	    	return true;
+	    }
+	    else {
+	    	
+	                for(int j=0;j<list.size()/2;j++) {
+	                   int sum = list.get(j);
+	                   listtwo.add(sum);
+	                }
+
+	                for(int j=list.size()-1;j>=list.size()/2;j--) {
+	                   int sum = list.get(j);
+	                   listthree.add(sum);
+	                }
+	                count=0;
+	                for(int j=0;j<listtwo.size();j++) {
+	                   if(listtwo.get(j).equals(listthree.get(j))){
+	                      count++;
+	                   }
+	                }
+	                if(count==listtwo.size()) {
+	                   return true;
+	                }
+	                return false;
+	             }
+
+	    }
+	 
+		
 }
-
-
-	public class MainClass {
-	    public static String booleanToString(boolean input) {
-	        return input ? "True" : "False";
-	    }
-	    
-	    public static void main(String[] args) throws IOException {
-	        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	        String line;
-	        while ((line = in.readLine()) != null) {
-	            int x = Integer.parseInt(line);
-	            
-	            boolean ret = new Solution().isPalindrome(x);
-	            
-	            String out = booleanToString(ret);
-	            
-	            System.out.print(out);
-	        }
-	    }
-	}
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode head = stringToListNode(line);
+            
+            boolean ret = new Solution().isPalindrome(head);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
